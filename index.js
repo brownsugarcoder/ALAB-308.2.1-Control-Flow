f2cf36668c08045b8818470a6b14d09312d528ef
ShaVon H. <144467852+brownsugarcoder@users.noreply.github.com>
2024-09-23 12:04:58 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+if (x=)
+
+const PI = 3.1415;
+const area = PI * radius * radius;
+3.1415 * 5=
+
+ let x = 3.1415;
+
+const PI = 3.1415;
+const area = PI * radius * radius;
+plantspace=.08
+
+const totalplants= area/.08
+console.log( )
